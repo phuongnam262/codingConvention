c8ea0281a7e8016e60ddff590c9d4e866fc077fe
nam262 <zdpnam1@gmail.com>
2025-03-21 13:43:53 +0700

    CodeConvention.java

--- src/studentdemo/updatecode.java
+++ src/studentdemo/updatecode.java
@@ -1,59 +1,61 @@
 package studentdemo;
 
-import java.util.*;
 import java.awt.Color;
-import java.awt.Font;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JPanel;
-import javax.swing.JOptionPane;
-import javax.swing.UIManager;
-import javax.swing.plaf.FontUIResource;
 import javax.swing.table.DefaultTableModel;
+
 
 public class JFStudent extends javax.swing.JFrame {
 
-    List<Student> list = new ArrayList<Student>();
-    Student x;
-    private static int pos = 0;
-    private static int check = 0;
-    JPanel panel;
+    private List<Student> studentList = new ArrayList<>();
+    private Student currentStudent;
+    private static int currentPosition = 0;
+    private static int checkFlag = 0;
+    private JPanel panel;
 
     public JFStudent() {
         initComponents();
         this.jPanel1.setBackground(Color.GRAY);
-        list.add(new Student("1111", "Nguyen Anh", 20));
-        list.add(new Student("2222", "Le Duong", 20));
-        list.add(new Student("3333", "Le Long", 30));
-        list.add(new Student("4444", "Nguyen Minh", 25));
-        View();
-        ViewTable();
+
+        studentList.add(new Student("1111", "Nguyen Anh", 20));
+        studentList.add(new Student("2222", "Le Duong", 20));
+        studentList.add(new Student("3333", "Le Long", 30));
+        studentList.add(new Student("4444", "Nguyen Minh", 25));
+
+        viewStudent();
+        updateTable();
     }
 
-    public void View() {
-        x = list.get(pos);
-        this.txtID.setText(x.getID());
-        this.txtName.setText(x.getName());
-        this.txtAge.setText("" + x.getAge());
-        OnOff(true, false);
-
+  
+    public void viewStudent() {
+        currentStudent = studentList.get(currentPosition);
+        this.txtID.setText(currentStudent.getId());
+        this.txtName.setText(currentStudent.getName());
+        this.txtAge.setText(String.valueOf(currentStudent.getAge()));
+        toggleButtons(true, false);
     }
 
-    public void ViewTable() {
+   
+    public void updateTable() {
         DefaultTableModel model = (DefaultTableModel) this.tblStudent.getModel();
-        model.setNumRows(0);
-        int n = 1;
-        for (Student x : list) {
-            model.addRow(new Object[]{n++, x.getID(), x.getName(), x.getAge()});
+        model.setRowCount(0); // Xóa dữ liệu cũ trong bảng
+
+        int index = 1;
+        for (Student student : studentList) {
+            model.addRow(new Object[]{index++, student.getId(), student.getName(), student.getAge()});
         }
 
         this.tblStudent.setRowHeight(22);
-
     }
 
-    public void OnOff(boolean a, boolean b) {
-        this.btnSave.show(b);
-        this.btnCancel.show(b);
+    public void toggleButtons(boolean enableMain, boolean enableSaveCancel) {
+        this.btnSave.setVisible(enableSaveCancel);
+        this.btnCancel.setVisible(enableSaveCancel);
 
-        this.btnAdd.show(a);
-        this.btnEdit.show(a);
-        this.btnDel.show(a);
+        this.btnAdd.setVisible(enableMain);
+        this.btnEdit.setVisible(enableMain);
+        this.btnDel.setVisible(enableMain);
     }
+}
